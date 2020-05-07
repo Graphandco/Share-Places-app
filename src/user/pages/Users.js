dd20e61033bab671804a716e9627892a8696225f
Reggio <contact@graphandco.com>
2020-05-07 19:07:20 +0200

    Add UserItem

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h2>Users Page</h2>;
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Jane Doe',
+            image: 'https://randomuser.me/api/portraits/women/66.jpg',
+            places: 3,
+        },
+    ];
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
