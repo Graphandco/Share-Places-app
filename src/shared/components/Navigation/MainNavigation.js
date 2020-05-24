66b0e6b6fd9a4518fc3ababeec76fe056477bc68
Reggio <contact@graphandco.com>
2020-05-24 12:16:00 +0200

    Update git

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,35 +3,34 @@ import { Link } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
-import Backdrop from '../UIElements/Backdrop';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 import './MainNavigation.css';
 
 const MainNavigation = (props) => {
     const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-    const openDrawer = () => {
+    const openDrawerHandler = () => {
         setDrawerIsOpen(true);
     };
 
-    const closeDrawer = () => {
+    const closeDrawerHandler = () => {
         setDrawerIsOpen(false);
     };
 
     return (
         <React.Fragment>
-            {drawerIsOpen ? <Backdrop onClick={closeDrawer} /> : null}
-            {drawerIsOpen ? (
-                <SideDrawer>
-                    <nav className='main-navigation__drawer-nav'>
-                        <NavLinks />
-                    </nav>
-                </SideDrawer>
-            ) : null}
+            {drawerIsOpen ? <Backdrop onClick={closeDrawerHandler} /> : null}
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+                <nav className='main-navigation__drawer-nav'>
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+
             <MainHeader>
                 <button
                     className='main-navigation__menu-btn'
-                    onClick={openDrawer}
+                    onClick={openDrawerHandler}
                 >
                     <span />
                     <span />
